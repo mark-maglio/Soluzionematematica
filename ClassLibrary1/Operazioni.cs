f78eba330069f51453274b8f2b1b912ab60fed43
ITT Rosselli Rasetti <rosselli.rasetti@outlook.com>
2019-10-04 13:10:42 +0200

    Add project files.

---
+++ ClassLibrary1/Operazioni.cs
@@ -0,0 +1,59 @@
+﻿using System;
+
+namespace ClassLibrary1
+{
+    public class Operazioni
+    {
+        public static int Somma(int a, int b)
+        {
+            int s = a + b;
+            return s;
+        }
+        public static int Sottrazione(int a, int b)
+        {
+            int s = a - b;
+            return s;
+        }
+        public static int Moltiplicazione(int a, int b)
+        {
+            int m = a * b;
+            return m;
+        }
+        public static double Divisione(double n1, double n2)
+        {
+            if (n2 == 0)
+            {
+                throw new Exception("N2 = 0");
+            }
+            double d = n1 / n2;
+            return d;
+        }
+        public static int Min(int a, int b)
+        {
+            if (a < b)
+            {
+                return a;
+            }
+            else return b;
+        }
+        public static int Max(int a, int b)
+        {
+            if (a > b)
+            {
+                return a;
+            }
+            else return b;
+        }
+        public static int Pot(int n1, int n2)
+        {
+            int p = 1 ;
+            int contatore = 0;
+            do
+            {
+                p = p * n1;
+                contatore++;
+            } while (contatore < n2);
+            return p;
+        }
+    }
+}
---
+++ matematica/Program.cs
@@ -0,0 +1,56 @@
+﻿using ClassLibrary1;
+using System;
+
+namespace matematica
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("inserisci un numero");
+            int n1 = int.Parse(Console.ReadLine());
+            Console.WriteLine("inserisci un'altro numero");
+            int n2 = int.Parse(Console.ReadLine());
+            try
+            {
+                double risultato = double.NaN;
+                Console.WriteLine("che operazione vuoi ?(s/m/d/a/min/max/pot)");
+                string risposta = Console.ReadLine();
+                if (risposta == "s")
+                {
+                    risultato = Operazioni.Sottrazione(n1, n2);
+                }
+                else if (risposta == "m")
+                {
+                    risultato = Operazioni.Moltiplicazione(n1, n2);
+                }
+                if (risposta == "d")
+                {
+                    risultato = Operazioni.Divisione(n1, n2);
+                }
+                else if (risposta == "a")
+                {
+                    risultato = Operazioni.Somma(n1, n2);
+                }
+                if (risposta == "min")
+                {
+                    risultato = Operazioni.Min(n1, n2);
+                }
+                if (risposta == "max")
+                {
+                    risultato = Operazioni.Max(n1, n2);
+                }
+                if (risposta == "pot")
+                {
+                    risultato = Operazioni.Pot(n1, n2);
+                }
+                Console.WriteLine($"{risultato}");
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex);
+            }
+            Console.ReadLine();
+        }
+    }
+}
